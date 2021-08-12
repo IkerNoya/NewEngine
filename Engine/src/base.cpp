9a44000ef0027c0d07c0b8e02ef047a1a596b5dc
IkerNoya <ikernoya4@gmail.com>
2021-08-12 13:12:32 -0300

    Update base.cpp

---
+++ Engine/src/base.cpp
@@ -0,0 +1,22 @@
+#include "base.h"
+using namespace Engine;
+
+Base::Base(){
+
+}
+
+Base::~Base(){
+
+}
+
+int Base::initEngine(){
+	return 0;
+}
+
+void Base::updateEngine(){
+
+}
+
+void Base::unloadEngine(){
+
+}
