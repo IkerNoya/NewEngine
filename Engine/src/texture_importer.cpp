d8f606ed96eacb45cb18bc304f5578af5b004c5f
Elias Brucart <eliasbrucart@hardgames.com.ar>
2021-10-11 22:27:32 -0300

    Added texture importer class

---
+++ Engine/src/texture_importer.cpp
@@ -0,0 +1,35 @@
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+#include "texture_importer.h"
+
+TextureImporter::TextureImporter() {
+
+}
+
+TextureImporter::TextureImporter(const char* path) {
+
+}
+
+TextureImporter::TextureImporter(int width, int height, const char* path, bool transparency) {
+
+}
+
+TextureImporter::~TextureImporter() {
+
+}
+
+void TextureImporter::LoadImage(int width, int height, bool transparency) {
+	
+}
+
+void TextureImporter::SetPath(const char* path) {
+
+}
+
+const char* TextureImporter::GetPath() {
+
+}
+
+unsigned int TextureImporter::GetTexture() {
+
+}
