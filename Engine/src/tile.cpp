bf69216667254ad82d3324dfb459b09561795f36
Iker <ikernoya4@gmail.com>
2021-11-26 11:03:38 -0300

    Completed tile class

--- Engine/src/tile.cpp
+++ Engine/src/tile.cpp
@@ -1,10 +1,34 @@
 #include "tile.h"
-
+#include "sprite.h"
 using namespace Engine;
 
-Tile::Tile() {
-	isWalkable = false;
-	index = 0;
+
+Tile::Tile() {}
+
+Tile::Tile(Sprite* texture, bool isWalkable) {
+	_tile = texture;
+	_isWalkable = isWalkable;
 }
 
-Tile::~Tile() {}
+Tile::~Tile() {
+	if (_tile != NULL) {
+		delete _tile;
+		_tile = NULL;
+	}
+}
+
+void Tile::SetTexture(Sprite* texture){
+	_tile = texture;
+}
+
+void Tile::SetIsWalkable(bool value) {
+	_isWalkable = value;
+}
+
+Sprite* Tile::GetTexture() {
+	return _tile;
+}
+
+bool Tile::GetIsWalkable() {
+	return _isWalkable;
+}
--- Engine/src/tile.h
+++ Engine/src/tile.h
@@ -1,17 +1,24 @@
 #ifndef TILE_H
 #define TILE_H
-
 #include "export.h"
-#include "texture_importer.h"
+
 namespace Engine {
+
+	class Sprite;
 	class ENGINE_API Tile {
+	private:
+		Sprite* _tile = nullptr;
+		bool _isWalkable = false;
+
 	public:
-		bool isWalkable;
-		int index;
-
 		Tile();
+		Tile(Sprite* texture, bool isWalkable);
 		~Tile();
+		void SetTexture(Sprite* texture);
+		void SetIsWalkable(bool value);
+		Sprite* GetTexture();
+		bool GetIsWalkable();
 	};
-}
 
+}
 #endif // !TILE_H
