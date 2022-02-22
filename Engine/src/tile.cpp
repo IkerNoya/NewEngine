b10828836101378cf8ea7de94897b5b8c9dfe295
Iker Noya <ikernoya4@gmail.com>
2022-02-22 20:16:13 -0300

    started creation of tile property reading tool

--- Engine/src/tile.cpp
+++ Engine/src/tile.cpp
@@ -1,10 +1,14 @@
 #include "tile.h"
+#include <vector>
+#include "xml_lib/tinyxml2.h"
+#include "utils.h"
+
 using namespace Engine;
 
 
 Tile::Tile() {}
 
-Tile::Tile(unsigned int id, bool isWalkable) : Sprite(){
+Tile::Tile(unsigned int id, bool isWalkable) : Sprite() {
 	_id = id;
 	_isWalkable = isWalkable;
 }
@@ -26,5 +30,16 @@ unsigned int Tile::GetID() {
 
 void Tile::SetID(unsigned int value) {
 	_id = value;
+}
+
+void Engine::Tile::SetPropertiesPath(const char* path)
+{
+	tinyxml2::XMLDocument map;
+	map.LoadFile(path);
+
+	tinyxml2::XMLElement* Tileset = map.FirstChildElement("tileset");
+	if (Tileset) {
+		printf("value: %f", Tileset->FloatAttribute("version"));
+	}
 }
 
